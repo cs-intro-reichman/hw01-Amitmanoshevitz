fb70c7e3597d8b5ecfc27a0ca8699fe9853a6544
Amit.manoshevitz <Amit.manoshevitz@post.runi.ac.il>
2023-12-21 13:23:32 +0200

    commit

--- AddTwo.java
+++ AddTwo.java
@@ -3,6 +3,15 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		// Put your code here	
+		public class AddTwo {
+			public static void main(String[] args) {
+			
+			int num1 = Integer.parseInt(args[0]);
+			int num2 = Integer.parseInt(args[1]);
+		
+			System.out.println(num1+num2);
+			}
+		}
+			
 	}
 }
