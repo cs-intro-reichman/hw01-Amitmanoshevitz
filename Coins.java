4c75fcf56718c1ff610a834631427fe1e9d71a3a
Amit.manoshevitz <Amit.manoshevitz@post.runi.ac.il>
2023-12-21 16:25:50 +0200

    new1

--- Coins.java
+++ Coins.java
@@ -3,13 +3,15 @@
  * The program prints how to represent this quantity using as many quarters as possible, plus the remainder in cents.
  */
 public class Coins {
-	public static void main(String[] args) {
-		
-		int num1 = Integer.parseInt(args[0]);
+    public static void main(String[] args) {
     
-		int c = num1%25 ;
-		int q = num1/25 ; 
-	
-		System.out.println(q  + " " + "quarters" + " " + c + " " + "cents");
-	}
+    int num1 = Integer.parseInt(args[0]);
+    
+    int c = num1%25 ;
+    int q = num1/25 ; 
+
+    System.out.println("Use " +q+ " quarters and "+ c + " " + "cents");
+    }
 }
+
+
